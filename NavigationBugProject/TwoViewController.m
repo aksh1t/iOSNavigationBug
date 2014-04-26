85feb654763bef0ece1bb6cd152f0d4059d04bb8
Matt Neuburg <matt@tidbits.com>
2014-04-26 09:36:23 -0700

    simplify

--- NavigationBugProject/TwoViewController.m
+++ NavigationBugProject/TwoViewController.m
@@ -15,39 +15,20 @@
 
 @implementation TwoViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.title = @"Two Title";
     
-    if ([self.navigationController respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
-        self.navigationController.interactivePopGestureRecognizer.enabled = YES;
-        self.navigationController.interactivePopGestureRecognizer.delegate = self;
-    }
-    // Do any additional setup after loading the view from its nib.
 }
 
 -(void)viewWillAppear:(BOOL)animated{
     [self.navigationController setNavigationBarHidden:NO animated:NO];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 - (IBAction)nextClick:(id)sender {
-    ThreeViewController *tvc = [[ThreeViewController alloc]initWithNibName:@"ThreeViewController" bundle:nil];
+    ThreeViewController *tvc = [ThreeViewController new];
     [self.navigationController pushViewController:tvc animated:YES];
 }
 
